d6bfc691ae7560d4364384d2187d5e387e16da75
Kruller Kristóf <kruller.kristof@mbvk.loc>
2023-04-24 14:09:10 +0200

    crud

--- main.go
+++ main.go
@@ -26,9 +26,58 @@ var movies []Movie
 
 // CRUD functions
 
-func getMovies(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(movies)
+func getMovies(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(movies)
+}
+
+func getMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(req)
+	for _, mov := range movies {
+		if mov.ID == params["id"] {
+			json.NewEncoder(res).Encode(mov)
+			return
+		}
+	}
+}
+
+func deleteMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(req)
+	for idx, mov := range movies {
+		if mov.ID == params["id"] {
+			movies = append(movies[:idx], movies[idx+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(res).Encode(movies)
+}
+
+func createMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(req.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(10000000000))
+	movies = append(movies, movie)
+	json.NewEncoder(res).Encode(movie)
+}
+
+func updateMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(req)
+	for idx, mov := range movies {
+		if mov.ID == params["id"] {
+			movies = append(movies[:idx], movies[idx+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(req.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(res).Encode(movie)
+			return
+		}
+	}
+
 }
 
 func main() {
